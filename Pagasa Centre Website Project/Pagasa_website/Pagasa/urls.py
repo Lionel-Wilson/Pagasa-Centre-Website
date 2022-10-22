211bdb5edd0dcf0d99cd91e16c49bed54113496a
Lionel Wilson <Lionel_Wilson@outlook.com>
2022-10-22 07:33:29 +0100

    implements add member feature

--- Pagasa Centre Website Project/Pagasa_website/Pagasa/urls.py
+++ Pagasa Centre Website Project/Pagasa_website/Pagasa/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('Home', views.index, name='home'),
     path('Information', views.info, name='info'),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord')
 ]
--- Pagasa Centre Website Project/Pagasa_website/Pagasa/views.py
+++ Pagasa Centre Website Project/Pagasa_website/Pagasa/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Members
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -9,10 +11,21 @@ def index(request):
     mymembers = Members.objects.all().values()
     template = loader.get_template('Home.html')
     context = {
-        'mymembers':mymembers,
+        'mymembers': mymembers,
     }    
     return HttpResponse(template.render(context, request))
 
 def info(request):
     template = loader.get_template('Information.html')
     return HttpResponse(template.render())
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['first']
+  y = request.POST['last']
+  member = Members(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
