2576abb3c6295594204be5934cb8f922988ad96d
Lionel Wilson <Lionel_Wilson@outlook.com>
2022-10-22 07:06:44 +0100

    displays church members on homepage

--- Pagasa Centre Website Project/Pagasa_website/Pagasa/views.py
+++ Pagasa Centre Website Project/Pagasa_website/Pagasa/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 # Create your views here.
 def index(request):
+    #EXAMPLE of displaying Model/DB data to view.
+    mymembers = Members.objects.all().values()
     template = loader.get_template('Home.html')
-    return HttpResponse(template.render())
+    context = {
+        'mymembers':mymembers,
+    }    
+    return HttpResponse(template.render(context, request))
 
 def info(request):
     template = loader.get_template('Information.html')
