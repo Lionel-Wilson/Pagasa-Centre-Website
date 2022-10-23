06820051049ab3775d67e6b9d33359e053c3d14c
Lionel Wilson <Lionel_Wilson@outlook.com>
2022-10-23 01:44:00 +0100

    add edit members feature

--- Pagasa Centre Website Project/Pagasa_website/Cell_Group/urls.py
+++ Pagasa Centre Website Project/Pagasa_website/Cell_Group/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add/', views.add, name='add'),
     path('add/addrecord/', views.addrecord, name='addrecord'),
     path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),#sends page to edit/update the record of member 
+    path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),#updates the database and redirects back to member list
 ]
--- Pagasa Centre Website Project/Pagasa_website/Cell_Group/views.py
+++ Pagasa Centre Website Project/Pagasa_website/Cell_Group/views.py
@@ -33,3 +33,20 @@ def delete(request, id):
     member.delete()
     #return back to the cell group page.
     return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymember': mymember
+    }
+    return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+  first = request.POST['first']
+  last = request.POST['last']
+  member = Members.objects.get(id=id)
+  member.firstname = first
+  member.lastname = last
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
