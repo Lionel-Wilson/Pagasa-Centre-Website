d632ce255364ae043aa6cabc4c83c2caeb94e739
Lionel Wilson <Lionel_Wilson@outlook.com>
2022-12-31 19:48:52 +0000

    intergrate django with react

--- Pagasa Centre Website Project/Pagasa_website/Pagasa_website/settings.py
+++ Pagasa Centre Website Project/Pagasa_website/Pagasa_website/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -65,7 +66,9 @@ ROOT_URLCONF = 'Pagasa_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'reactfrontend/build'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'reactfrontend/build/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Pagasa Centre Website Project/Pagasa_website/reactfrontend/src/App.js
+++ Pagasa Centre Website Project/Pagasa_website/reactfrontend/src/App.js
@@ -4,20 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="navbar">
+            <a >Home</a>
+            <a >Info</a>
+            <a >Gallery</a>
+            <div className="subnav">
+                <button className="subnavbtn">Archive<i className="fa fa-caret-down"></i></button>
+                <div id="subnav-evang-content">
+                    <a>Evangelistic Night</a>
+                    <a>Bible Study</a>
+                    <a>Wildsons</a>
+                    <a>Cell Celebration</a>
+                    <a>SOL &amp; LifeClass</a>
+                </div>
+            </div>
+            <div className="subnav">
+                <button className="subnavbtn">Cell Group<i className="fa fa-caret-down"></i></button>
+                <div id="subnav-cellgroup-content">
+                    <a>VIP</a>
+                    <a>Need a cell</a>
+                    <a>Cell Leaders</a>
+                </div>
+            </div>
+            <a>Students</a>
+            <a>Sunday School</a>
+        </div>
+            
     </div>
   );
 }
