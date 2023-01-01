8acce9fbc7b94433887091dd0e3f7e531f45900b
Lionel Wilson <Lionel_Wilson@outlook.com>
2023-01-01 00:37:15 +0000

    clean up reactfrontend

---
+++ Pagasa Centre Website Project/Pagasa_website/reactfrontend/src/components/Home.js
@@ -0,0 +1,34 @@
+import '../css/Home.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <a >Home</a>
+        <a >Info</a>
+        <a >Gallery</a>
+        <div className="subnav">
+            <button className="subnavbtn">Archive<i className="fa fa-caret-down"></i></button>
+            <div id="subnav-evang-content">
+                <a>Evangelistic Night</a>
+                <a>Bible Study</a>
+                <a>Wildsons</a>
+                <a>Cell Celebration</a>
+                <a>SOL &amp; LifeClass</a>
+            </div>
+        </div>
+        <div className="subnav">
+            <button className="subnavbtn">Cell Group<i className="fa fa-caret-down"></i></button>
+            <div id="subnav-cellgroup-content">
+                <a>VIP</a>
+                <a>Need a cell</a>
+                <a>Cell Leaders</a>
+            </div>
+        </div>
+        <a>Students</a>
+        <a>Sunday School</a>
+    </div>
+
+  );
+}
+
+export default Navbar;
