930f57501fd1ae72d978fbf5ea831356d83f0f65
Lionel Wilson <Lionel_Wilson@outlook.com>
2023-01-01 04:34:39 +0000

    basic bootstrap menu

--- Pagasa Centre Website Project/Pagasa_website/frontend/src/components/Nav.js
+++ Pagasa Centre Website Project/Pagasa_website/frontend/src/components/Nav.js
@@ -2,9 +2,47 @@ import '../css/Nav.css';
 
 function Nav() {
   return (
-    <div className="Nav">
-      <p> Hello world</p>
-    </div>
+    <nav class="navbar">
+      <a class="navbar-brand">
+        <img src="../static/images/pagasa logo.png" width="100" height="100"></img>
+      </a>
+      
+      <ul class="nav justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Information</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gallery</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Archive</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Bible Study</a>
+            <a class="dropdown-item" href="#">Wildsons</a>
+            <a class="dropdown-item" href="#">Cell Celebration</a>
+            <a class="dropdown-item" href="#">SOL &amp; LifeClass</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Cell Group</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">VIP</a>
+            <a class="dropdown-item" href="#">Need a cell</a>
+            <a class="dropdown-item" href="#">Cell Celebration</a>
+            <a class="dropdown-item" href="#">Cell Leaders</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sunday School</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
