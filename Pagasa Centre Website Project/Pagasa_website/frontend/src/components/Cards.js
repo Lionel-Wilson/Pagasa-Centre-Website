0a1cb9c1a9f2da3d1664686004a717d7850446a3
Lionel Wilson <Lionel_Wilson@outlook.com>
2023-01-01 18:03:59 +0000

    remove margings

--- Pagasa Centre Website Project/Pagasa_website/frontend/src/components/Cards.js
+++ Pagasa Centre Website Project/Pagasa_website/frontend/src/components/Cards.js
@@ -10,10 +10,10 @@ function Cardsection(props) {
         <Card className="bg-dark text-white">
           <Card.Img src={props.image}/>
           <Card.ImgOverlay className='d-flex align-items-center justify-content-center flex-column mb-3 ' >
-            <Card.Title className='text-center display-1 fw-bold'>{props.main_text}</Card.Title>
+            <Card.Title className='text-center display-1 fw-bold mb-0'>{props.main_text}</Card.Title>
             <br></br>
 
-            <Card.Subtitle className='text-center'><h4>{props.sub_text}</h4></Card.Subtitle>
+            <Card.Subtitle className='text-center'><h4 class='mb-0'>{props.sub_text}</h4></Card.Subtitle>
             <br></br>
             
             <div className='d-flex align-items-center justify-content-center '>
@@ -65,10 +65,12 @@ function Cardsection(props) {
       <Card className="bg-dark text-white">
         <Card.Img src={props.image}/>
         <Card.ImgOverlay className='d-flex align-items-center justify-content-center flex-column mb-3 ' >
-          <Card.Title className='text-center display-1 fw-bold'>{props.main_text}</Card.Title>
+          <Card.Title className='text-center display-1 fw-bold mb-0'>{props.main_text}</Card.Title>
           <br></br>
-          <Card.Subtitle className='text-center'><h4>{props.sub_text}</h4></Card.Subtitle>
+
+          <Card.Subtitle className='text-center'><h4 className='mb-0'>{props.sub_text}</h4></Card.Subtitle>
           <br></br>
+
           <div class="text-center">
             <Button className='btn text-center' variant="light" size="lg" type="button"><p class="mb-0 fs-2 ">{props.button_text}</p></Button>
           </div>
